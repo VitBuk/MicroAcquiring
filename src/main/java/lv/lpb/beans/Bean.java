e12a51d24e85db7437502bf1fd2e80c3536fba3e
Vitalijs Bukovskijs <vitbuk@bank.lpb>
2015-12-15 17:52:51 +0200

    find/update/delete/getAll for beans

---
+++ src/main/java/lv/lpb/beans/Bean.java
@@ -0,0 +1,16 @@
+package lv.lpb.beans;
+
+import java.util.List;
+
+public interface Bean<T> {
+    
+    public void persist(T t);
+    
+    public T find(Long id);
+    
+    public void update(T t);
+    
+    public void delete (Long id);
+    
+    public List<T> getAll();
+}
--- src/main/java/lv/lpb/beans/MerchantBean.java
+++ src/main/java/lv/lpb/beans/MerchantBean.java
@@ -10,36 +10,39 @@ import javax.persistence.PersistenceContext;
 import lv.lpb.domain.Merchant;
 
 @Stateless
-public class MerchantBean {
+public class MerchantBean implements Bean<Merchant> {
     
     @PersistenceContext(unitName = "MySql")
     EntityManager entityManager;
     
+    @Override
     public void persist(Merchant merchant) {
         entityManager.persist(merchant);
     }
     
+    @Override
     public Merchant find(Long id) {
         return entityManager.find(Merchant.class, id);
     }
     
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
-    public void update (Long id, Merchant.Status status) {
-        Merchant merchant = find(id);
-        entityManager.lock(merchant, LockModeType.OPTIMISTIC);
-        merchant.setStatus(status);
-        entityManager.merge(merchant);
+    @Override
+    public void update (Merchant merchant) {
+        Merchant merchantFromDB = find(merchant.getId());
+        entityManager.lock(merchantFromDB, LockModeType.OPTIMISTIC);
+        merchantFromDB.setStatus(merchant.getStatus());
+        entityManager.merge(merchantFromDB);
         entityManager.flush();
     }
     
+    @Override
     public void delete (Long id) {
         entityManager.remove(find(id));
     }
     
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @Override
     public List<Merchant> getAll() {
         return entityManager.createNamedQuery("Merchant.findAll", Merchant.class).getResultList();
     }
-    
-    
 }
